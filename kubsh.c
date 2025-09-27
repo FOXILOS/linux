97ee3b861ad66f014ec32bbace61d620d81fa00b
Alina <anitalozova@gmail.com>
2025-09-27 13:44:48 +0300

    123

--- kubsh.c
+++ kubsh.c
@@ -10,8 +10,20 @@ while(1){
 		break;
 	}
 	printf("%s: command not found\n", input);
-
+	if(strstr(input, "debug \"") == input) {
+		char *start = strchr(input, '\"');
+		char *end = strchr(start + 1, '\"');
+		if(start != NULL && end != NULL) {
+			printf("DEBUG: ");
+			for(char *p = start + 1; p < end; p++){
+				putchar(*p);
+			}
+			printf("\n");
+		}
+	}else{
+		printf("%s: command not found\n", input);
+	}
 }
-
  return 0;
 }
+
